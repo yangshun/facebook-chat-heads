9a8911da82ca2b08501a368e38273c225f5664d8
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-03-01 20:35:32 +0800

    Initial Commit

---
+++ Facebook Chat Heads/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Facebook Chat Heads
+//
+//  Created by YangShun on 1/3/14.
+//  Copyright (c) 2014 YangShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Facebook Chat Heads/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Facebook Chat Heads
+//
+//  Created by YangShun on 1/3/14.
+//  Copyright (c) 2014 YangShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
